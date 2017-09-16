8105b4f9bb7a81a70d356ed668a46d15f49b4279
Victor Grishchenko <victor.grishchenko@gmail.com>
2017-09-16 09:06:58 +0300

    simple UUID to UUID map

---
+++ umap.go
@@ -0,0 +1,40 @@
+package RON
+
+type UUID2Map struct {
+	subs map[UUID][]UUID
+	// avoid allocating lots of small slices, allocate larger slabs
+	slab []UUID
+}
+// min RAM use per object: 16b key, 16b slice, 16*2b on the slab = 64
+
+const U2M_DEFAULT_SLICE_SIZE = 2
+const U2M_SLAB_SIZE = 32 // 32*16=512 bytes
+
+func MakeUUID2Map() UUID2Map {
+	um := UUID2Map{
+		subs: make(map[UUID][]UUID),
+		slab: make([]UUID, U2M_SLAB_SIZE),
+	}
+	return um
+}
+
+func (um *UUID2Map) Add(key, value UUID) {
+	values, ok := um.subs[key]
+	if !ok {
+		if len(um.slab) < U2M_DEFAULT_SLICE_SIZE {
+			um.slab = make([]UUID, U2M_SLAB_SIZE)
+		}
+		values = um.slab[0:0:U2M_DEFAULT_SLICE_SIZE]
+		um.slab = um.slab[U2M_DEFAULT_SLICE_SIZE:]
+	}
+	values = append(values, value)
+	um.Put(key, values)
+}
+
+func (um UUID2Map) List(key UUID) []UUID {
+	return um.subs[key]
+}
+
+func (um *UUID2Map) Put(key UUID, values []UUID) {
+	um.subs[key] = values
+}
---
+++ umap_test.go
@@ -0,0 +1,25 @@
+package RON
+
+import "testing"
+
+func TestUUID2Map_List(t *testing.T) {
+	um := MakeUUID2Map()
+	um.Add(NEVER_UUID, ZERO_UUID)
+	um.Add(NEVER_UUID, NEVER_UUID)
+	for i := 0; i < 100; i++ {
+		um.Add(ZERO_UUID, UUID{uint64(i), UUID_NAME_UPPER_BITS})
+	}
+	nv := um.List(NEVER_UUID)
+	if len(nv) != 2 || nv[0] != ZERO_UUID || nv[1] != NEVER_UUID {
+		t.Fail()
+	}
+	hu := um.List(ZERO_UUID)
+	if len(hu) != 100 {
+		t.Fail()
+	}
+	for i := 0; i < 100; i++ {
+		if hu[i].Value != uint64(i) {
+			t.Fail()
+		}
+	}
+}
