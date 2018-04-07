c9c1cc630989b5c61638cd1186ae76c871353e8f
Victor Grishchenko <victor.grishchenko@gmail.com>
2018-04-07 18:31:45 +0300

    cli test tool

---
+++ ron/ron.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main () {
+	flag.Parse()
+	args := flag.Args()
+	if len(args)==0 {
+		return
+	}
+	cmd := args[0]
+	args = args[1:]
+	switch cmd {
+	case "convert":
+		convert(args)
+	default:
+		Misunderstood("command unknown")
+	}
+}
+
+func Misunderstood(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(7)
+}
